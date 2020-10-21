639d866cd15011d6ca9aab5907e99c203feeee23
Samson Chisom <samsondestined@gmail.com>
2020-10-21 11:09:39 +0100

    Add files via upload

--- audit-trail.php
+++ audit-trail.php
@@ -14,19 +14,75 @@ class Crud
 			$this->conn = $connectionObject;
 		}
 
+		//Select Data From Any Table
+		public function select($table, $rows='*', $where=null, $order=null, $group =null, $join =null, $limit=null)
+		{  
+			try 
+			{
+				$sql="SELECT $rows FROM $table"; 
+
+				if($join!=null)
+				{
+					$sql .= $join;
+				}
+
+				if($where!=null)
+				{
+					$sql .= " WHERE " . $where;
+				}
+
+				if($group!=null)
+				{
+					$sql .= " GROUP BY " . $group;
+				}
+
+				if($order!=null)
+				{
+					$sql .= " ORDER BY " . $order;
+				}
+
+				if($limit!=null)
+				{
+					$sql .= " LIMIT " . $limit;
+				}
+				
+				$q = $this->conn->prepare($sql);  
+				$q->execute();
+
+				if($q->rowCount() > 0)
+				{
+					while($r = $q->fetch(PDO::FETCH_ASSOC))
+					{ 
+						$data[]=$r;
+					} 
+					return $data; 
+				}
+
+				else
+				{
+					return false;
+				}
+			} 
+
+			catch (PDOException $e) 
+			{
+				echo $e->getMessage();
+			}
+        } 
+        
+        //INSERT AND UPDATE ON TABLE1
 		public function LogAllCalculate($identifier, $userid, $fullname)
 		{
 			try 
 			{	 
-				$check = "SELECT * FROM general_log WHERE IPAddr = '".$_SERVER["REMOTE_ADDR"]."' 
+				$check = "SELECT * FROM table1 WHERE IPAddr = '".$_SERVER["REMOTE_ADDR"]."' 
 										&& UserID = '".$userid."'";
 				$q = $this->conn->prepare($check);
 				$q->execute();
-				//$q->fetch(PDO::FETCH_ASSOC);
 
 				if($q->rowCount() < 1)
 				{
-					$sqlx = "INSERT INTO general_log SET Identifier = :identifier, 
+					$sqlx = "INSERT INTO table1 SET Identifier = :identifier, 
 												UserID = :userid, 
 												FullName = :fullname, 
 												Visit = :visit, 
@@ -44,7 +100,7 @@ class Crud
 					$m = $q->fetch(PDO::FETCH_ASSOC);
 					$visit = ($m["Visit"] + 1);
 
-					$sqlz = "UPDATE general_log SET Visit = :visit, 
+					$sqlz = "UPDATE table1 SET Visit = :visit, 
 													Identifier = :identifier, 
 													FullName = :fullname 
 							WHERE IPAddr = '".$_SERVER["REMOTE_ADDR"]."' 
@@ -65,7 +121,7 @@ class Crud
 			
 		}
 
-
+        //INSERT TO TABLE2
 		public function LogAudit($identifier, $userid, $fullname)
 		{
 			$Timenow = date("Y-m-d G:i:s");
@@ -73,7 +129,7 @@ class Crud
 			{	
 				$url = $_SERVER["REQUEST_URI"];
 
-				$sqlx = "INSERT INTO audit_log SET Task = :task, 
+				$sqlx = "INSERT INTO table2 SET Task = :task, 
 											UserID = :userid, 
 											FullName = :fullname,
 											Identifier = :identifier, 
@@ -98,12 +154,9 @@ class Crud
 }
 
 
-
-
 //CONTROLS
 function FetchTableContent($i)
 {
-
 	global $connect;
 	$crud = new Crud($connect);
 
@@ -111,24 +164,24 @@ function FetchTableContent($i)
 
 		//select total unique users from general_log table
 		case 10:
-			$data = $crud->select("general_log","COUNT(ID) as Total", "", "ID DESC");
+			$data = $crud->select("table1","COUNT(ID) as Total", "", "ID DESC");
 			return $data[0]["Total"];
 			break;
 
 		//select total clicks from general_log table
 		case 11:
-			$data = $crud->select("general_log","SUM(Visit) as Total", "", "ID DESC");
+			$data = $crud->select("table1","SUM(Visit) as Total", "", "ID DESC");
 			return $data[0]["Total"];
 			break;
 
 		//select total unique user clicks from general_log table(For Individual Dashboards)
 		case 12:
-			$data = $crud->select("general_log","COUNT(Visit) as Total", "", "ID DESC");
+			$data = $crud->select("table","COUNT(Visit) as Total", "", "ID DESC");
 			return $data[0]["Total"];
 
 		//select all row from Audit Log
 		case 13:
-			$data = $crud->select("audit_log","*", "", "COUNT(ID) DESC", "UserID");
+			$data = $crud->select("table2","*", "", "COUNT(ID) DESC", "UserID");
 			return $data;
 			break;
 
@@ -165,7 +218,7 @@ $home -> LogAudit($identifier, $userid, $fullname);
 
 
 
-//PUT THIS IN THE ADMIN DASHBOARD TO READ AND UPDATE AUDIT TRAIL
+//PUT THIS IN THE ADMIN DASHBOARD OR ANY WHERE YOU WANT THE AUDIT TRAIL INFORMATION TO SHOW
 //Total Clicks
  echo FetchTableContent(11); 
 //Active Users
